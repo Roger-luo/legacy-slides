ebf492ad70dfdc29d2f865b74a4b896ab6f486e1
Roger-luo <hiroger@qq.com>
2016-04-15 00:18:11 +0800

    update contents 1

---
+++ gen.jl
@@ -0,0 +1,59 @@
+function str2cmdstr(str::AbstractString)
+    split(str,r"\s+")
+    convert(Vector{ByteString},split(str,r"\s+"))
+end
+
+function str2cmdstr(c::Vector{ByteString},str::AbstractString)
+    append!(c,str2cmdstr(str))
+end
+
+function pandoc(
+    filename::AbstractString;
+    o::Union{AbstractString,Void}=nothing,
+    toc::Bool=false,
+    toc_depth::Integer=2,
+    standalone::Bool=false,
+    revealjs::Bool=false,
+    template::Union{AbstractString,Void}=nothing
+    )
+    iname = match(r"(.*).md",filename).captures[1]
+    
+    @show filename
+    @show iname
+
+    args = str2cmdstr("pandoc $(iname).md -o")
+
+    o!=nothing?str2cmdstr(args,"$(iname).html"):o
+
+    toc==true?str2cmdstr(args,"--toc --toc-depth=$(toc_depth)"):nothing
+
+    # By default, pandoc produces a document fragment, not a standalone docu‐
+    # ment with a proper header and footer.  To produce  a  standalone  docu‐
+    # ment, use the -s or --standalone flag:
+    standalone==true?str2cmdstr(args,"-s"):nothing
+
+    revealjs==true?str2cmdstr(args,"-t revealjs"):nothing
+
+    template!=nothing?str2cmdstr(args,"--template=$(template)"):nothing
+
+    run(Cmd(args))
+end
+
+function init()
+    return ""
+end
+
+function gen()
+    pandoc("README.md";o="index.html",toc=true,toc_depth=2,template="index.revealjs")
+    for file in readdir("_contents")
+        file = match(r"(.*).md",file)
+        @assert file!=nothing
+        pandoc("_contents/$(file.captures[1]).md";o="contents/$(file).md",revealjs=true,standalone=true,template="slide.revealjs")
+    end
+
+    run(`git add *`)
+    run(`git commit -m"update contents $1"`)
+    run(`git push origin gh-pages`)
+end
+
+gen()
